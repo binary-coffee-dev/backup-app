919d404aaeae6b5c3ef26a4c9105c1e1997f222d
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-06-07 17:02:37 +0200

    Create setup store script

---
+++ setup-store.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+function main() {
+    const {Storage} = require('@google-cloud/storage');
+
+    const keyFile = path.join(__dirname, 'store-key.json');
+    const storage = new Storage({keyFilename: keyFile, projectId: 'eternal-psyche-279612'});
+
+    const bucketsList = [
+        'bc-database-backup',
+        'bc-images-backup'
+    ];
+
+    async function createBuckets() {
+        const [buckets] = await storage.getBuckets();
+        const mark = new Set();
+        buckets.forEach(b => mark.add(b.id));
+        for (let b of bucketsList) {
+            if (!mark.has(b)) {
+                await storage.createBucket(b);
+            }
+        }
+    }
+
+    createBuckets().catch(console.error);
+}
+
+main(...process.argv.slice(2));
